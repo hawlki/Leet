1683e21ce5f7242a05b53532c4267ebb59320e96
israel lopes <israel.lopes2710@gmail.com>
2021-03-21 16:33:52 -0300

    organizxa arquivo

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ namespace Leet
         {
             Console.Clear();
             Console.Beep();
+
             Console.WriteLine("-----------------------------");
             Console.WriteLine("Etec Adolpho Berezin");
             Console.WriteLine("Feito por Israel Camilo Lopes");
             Console.WriteLine("1I3");
             Console.WriteLine("-----------------------------");
+
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.Write("Digite uma mensagem:");
             String mensagem = Console.ReadLine();
@@ -30,6 +32,7 @@ namespace Leet
                                .Replace("T","7")
                                .Replace("s","5")
                                .Replace("S","5");
+
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("-----------------------------");
             Console.BackgroundColor = ConsoleColor.White;
@@ -37,8 +40,10 @@ namespace Leet
             Console.WriteLine($"{mensagem}");
             Console.ResetColor();
             Console.WriteLine("-----------------------------");
+
             Console.WriteLine("Escolha uma tecla e clique nela para limpar sua tela");
             Console.ReadKey();
+            
             Console.Clear();
         }
     }
